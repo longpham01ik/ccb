125566e64d30cffcd822414265c048a2ac433bc2
longpham01ik <you@example.com>
2022-04-15 19:27:57 +0700

    fix database

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
-define('DB_NAME', 'shopping');
+define('DB_SERVER','3.132.234.157');
+define('DB_USER','long34');
+define('DB_PASS' ,'123@123a');
+define('DB_NAME', 'long34');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
