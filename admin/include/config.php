8257b67dfe7cb9ca8e5670293c1cd5f5521faa97
longpham01ik <89621817+longpham01ik@users.noreply.github.com>
2022-04-16 09:34:06 +0700

    Update config.php

--- admin/include/config.php
+++ admin/include/config.php
@@ -2,7 +2,7 @@
 define('DB_SERVER','3.132.234.157');
 define('DB_USER','long34');
 define('DB_PASS' ,'123@123a');
-define('DB_NAME', 'long34');
+define('DB_NAME', 'shopping');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
